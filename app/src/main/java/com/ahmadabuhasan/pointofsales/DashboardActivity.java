81b488c911cdff3ffc209279565ed3152b830391
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2025-12-26 13:37:34 +0700

    Refactor: Remove unused SyncStats import
    This commit removes an unused import of `android.content.SyncStats` from `DashboardActivity.java`.

--- app/src/main/java/com/ahmadabuhasan/pointofsales/DashboardActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsales/DashboardActivity.java
@@ -3,7 +3,6 @@ package com.ahmadabuhasan.pointofsales;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SyncStats;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
