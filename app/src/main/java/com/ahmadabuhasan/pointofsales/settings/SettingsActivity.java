971bd85272bc42547762fc5af6816beaf27909a1
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2025-12-26 16:42:44 +0700

    Feature: Add database reset functionality to SettingsActivity
    This commit introduces a "Reset" option in the `SettingsActivity` menu, allowing users to clear all application data.
    
    Key changes:
    - Implemented `onCreateOptionsMenu` to inflate `R.menu.setting_menu`.
    - Updated `onOptionsItemSelected` to handle the `R.id.menu_reset` action.
    - Added a confirmation `AlertDialog` before invoking `databaseAccess.clearAllData()`.
    - Added a success notification using `Toasty` upon successful data deletion.

--- app/src/main/java/com/ahmadabuhasan/pointofsales/settings/SettingsActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsales/settings/SettingsActivity.java
@@ -2,11 +2,14 @@ package com.ahmadabuhasan.pointofsales.settings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import com.ahmadabuhasan.pointofsales.R;
+import com.ahmadabuhasan.pointofsales.database.DatabaseAccess;
 import com.ahmadabuhasan.pointofsales.databinding.ActivitySettingsBinding;
 import com.ahmadabuhasan.pointofsales.settings.backup.BackupActivity;
 import com.ahmadabuhasan.pointofsales.settings.categories.CategoriesActivity;
@@ -18,6 +21,8 @@ import com.ahmadabuhasan.pointofsales.utils.BaseActivity;
 import com.ahmadabuhasan.pointofsales.utils.Utils;
 
 import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
 
 /*
  * Created by Ahmad Abu Hasan (C) 2022
@@ -52,7 +57,31 @@ public class SettingsActivity extends BaseActivity {
         if (item.getItemId() == android.R.id.home) {
             this.finish();
             return true;
+        } else if (item.getItemId() == R.id.menu_reset) {
+            final DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+            databaseAccess.open();
+            new AlertDialog.Builder(this)
+                    .setCancelable(false)
+                    .setTitle(getString(R.string.confirmation))
+                    .setMessage(getString(R.string.are_you_sure_you_want_to_delete_all_data))
+                    .setCancelable(false)
+                    .setPositiveButton(getString(R.string.yes), (dialog, which) -> {
+                        databaseAccess.clearAllData();
+                        Toasty.success(this, R.string.all_data_deleted_successfully, Toasty.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    })
+                    .setNegativeButton(getString(R.string.no), (dialog, which) -> {
+                        databaseAccess.close();
+                        dialog.dismiss();
+                    })
+                    .show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
